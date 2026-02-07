d80425bbb032688d421abe323e58db6d85d341ae
nabbisen <nabbisen@scqr.net>
2026-02-07 20:56:44 +0900

    dir_node defautl with path

--- src/core/dir_node.rs
+++ src/core/dir_node.rs
@@ -7,3 +7,13 @@ pub struct DirNode {
     pub sub_dirs: Vec<DirNode>, // subdirectories (recursion)
     pub files: Vec<PathBuf>,    // files
 }
+
+impl DirNode {
+    pub fn with_path(path: PathBuf) -> Self {
+        Self {
+            path,
+            sub_dirs: vec![],
+            files: vec![],
+        }
+    }
+}
