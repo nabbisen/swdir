be9e6147e8ac52cc057d21706d3addd415609bb9
nabbisen <nabbisen@scqr.net>
2026-02-07 20:48:48 +0900

    Clone trait on dir_node

--- src/core/dir_node.rs
+++ src/core/dir_node.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
 /// directory tree
-#[derive(Debug, Eq, PartialEq, Ord, PartialOrd)]
+#[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
 pub struct DirNode {
     pub path: PathBuf,
     pub sub_dirs: Vec<DirNode>, // subdirectories (recursion)
