772b785ad077dff02a84fa49cf903f43c8e679dc
nabbisen <nabbisen@scqr.net>
2026-02-11 18:34:12 +0900

    examples; docs.rs entry

--- README.md
+++ README.md
@@ -9,8 +9,46 @@
 Swiftly traverse and scan directories recursively.
 Sway 💃, swing 🎷 or swim 🪼 in directories.
 
+## Quick start
+
 ```rust
 use swdir::Swdir;
 
-let dir_node = Swdir::default().set_root_path("/some/path").scan(); // -> DirNode
+let dir_node = Swdir::default().set_root_path("/some/path").scan();
+// -> DirNode (files and subdirectories)
+```
+
+### Recurse option
+
+```rust
+use swdir::{Recurse, Swdir};
+
+let recurse = Recurse {
+    enabled: true,
+    skip_hidden: true,    // skip hidden files and directories
+    depth_limit: Some(1), // only first level subdirectory is scanned
+};
+
+let dir_node = Swdir::default()
+    .set_root_path("/some/path")
+    .set_recurse(recurse)
+    .scan();
+```
+
+### Allowlist and denylist
+
+```rust
+use swdir::Swdir;
+
+let dir_node_with_allowlist = Swdir::default()
+    .set_root_path("/some/path")
+    .set_extension_allowlist(&["md"])
+    .unwrap()
+    .scan();
+
+let dir_node_with_denylist = Swdir::default()
+    .set_root_path("/some/path")
+    .set_extension_allowlist(&["md"])
+    .unwrap()
+    .scan();
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,46 @@
+/// ## Quick start
+///
+/// ```rust
+/// use swdir::Swdir;
+///
+/// let dir_node = Swdir::default().set_root_path("/some/path").scan();
+/// // -> DirNode (files and subdirectories)
+/// ```
+///
+/// ### Recurse option
+///
+/// ```rust
+/// use swdir::{Recurse, Swdir};
+///
+/// let recurse = Recurse {
+///     enabled: true,
+///     skip_hidden: true,    // skip hidden files and directories
+///     depth_limit: Some(1), // only first level subdirectory is scanned
+/// };
+///
+/// let dir_node = Swdir::default()
+///     .set_root_path("/some/path")
+///     .set_recurse(recurse)
+///     .scan();
+/// ```
+///
+/// ### Allowlist and denylist
+///
+/// ```rust
+/// use swdir::Swdir;
+///
+/// let dir_node_with_allowlist = Swdir::default()
+///     .set_root_path("/some/path")
+///     .set_extension_allowlist(&["md"])
+///     .unwrap()
+///     .scan();
+///
+/// let dir_node_with_denylist = Swdir::default()
+///     .set_root_path("/some/path")
+///     .set_extension_allowlist(&["md"])
+///     .unwrap()
+///     .scan();
+/// ```
 mod core;
 mod helpers;
 
