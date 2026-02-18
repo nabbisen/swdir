d26d2a1f52ebb4aa51304f0662ede25d716ed477
nabbisen <nabbisen@scqr.net>
2026-02-18 17:32:02 +0900

    is_hidden() fn integrated

--- src/helpers/file.rs
+++ src/helpers/file.rs
@@ -1,15 +1,20 @@
-#[cfg(not(windows))]
 /// check if file is hidden
 pub fn is_hidden(entry: &std::fs::DirEntry) -> bool {
-    entry.file_name().to_string_lossy().starts_with('.')
-}
+    let start_with_dot = entry.file_name().to_string_lossy().starts_with('.');
 
-#[cfg(windows)]
-/// check if file is hidden
-pub fn is_hidden(entry: &std::fs::DirEntry) -> bool {
-    use std::os::windows::fs::MetadataExt;
-    if let Ok(metadata) = entry.metadata() {
-        return metadata.file_attributes() & 0x2 != 0;
+    if start_with_dot {
+        return true;
     }
+
+    #[cfg(windows)]
+    {
+        use std::os::windows::fs::MetadataExt;
+        if let Ok(metadata) = entry.metadata() {
+            if metadata.file_attributes() & 0x2 != 0 {
+                return true;
+            }
+        }
+    }
+
     false
 }
